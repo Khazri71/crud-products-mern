b415e0e6a03bba8e8aa2c940cf1c22128d2dcc62
khazri71 <khazrinoura71@gmail.com>
2026-01-21 15:48:22 +0100

    add allowed origin

--- backend/src/config/configCors/allowedOrigins.ts
+++ backend/src/config/configCors/allowedOrigins.ts
@@ -1,4 +1,4 @@
 // const allowedOrigins : string [] = ["http://localhost:3000"]
-const allowedOrigins : string [] = [process.env.ALLOWED_ORIGIN!]
+const allowedOrigins : string [] = [process.env.ALLOWED_ORIGINS!]
 
 export default allowedOrigins
